9c823295b128094a7a020361d46f5f37712c7594
Karthik Abinav <karthikabinavs@gmail.com>
2008-11-19 09:00:11 +0530

    Fixed the inheritance issue

--- compilers Lab/lab 3/TypeChecking/testing.java
+++ compilers Lab/lab 3/TypeChecking/testing.java
@@ -1,21 +1,89 @@
+/*
+The Ultimate Test Case - Abhiram R
+
+*/
+
 class Test{
     public static void main(String[] a){
-        System.out.println(new A().run(2, new A().manju(), new A().abhiram(new A().manju(),3)));
+        System.out.println(new A().run(2, new A().goku(5), new A().abhiram(new A().goku(7), new A().yo(new A().yo(new A().abhiram3(new A().abhiram2(5)), new D().voila()) ,new B().btoc(true, new A().baba(true, false))))));
     }
 }
-class A {
+class A extends B{
     int[] a;
     int c;
+    //int what;
     public int run(int b, int d, int f){
         c = b;
+        yes = b;
+        what = f;
         return 0;
     }
     public int abhiram(int b, int d)
     {
         return 1;
     }
-    public int manju()
+    public int goku(int b)
     {
         return 4;
     }
+    public int yo(int g, int h)
+    {
+        return 3;
+    }
+    public C selfa()
+    {
+        return this;
+    }
+}
+class B extends C
+{
+    int blah;
+    int yes;
+    public boolean baba(boolean c, boolean d)
+    {
+        int a;
+        return true;
+    }
+    
+}
+class C
+{
+    int what;
+    A hello;
+    public int yo(int g, int h)
+    {
+        return 0;
+    }
+    public int btoc(boolean a, boolean b)
+    {
+    		boolean c;
+    		c = a;
+    		return 4;
+    }
+    public A abhiram2(int a)
+    {
+        hello = new A();
+    		return hello;
+    }
+    public int abhiram3(A a)
+    {
+    		return 5;
+    }
+    public B abhiram4()
+    {
+        return hello;
+    }
+}
+class D
+{
+	int hi;
+	B b;
+	C c;
+	public int voila()
+	{
+	    b = new A();
+	    c = new B();
+	    c = new A();
+		return hi;
+	}
 }
