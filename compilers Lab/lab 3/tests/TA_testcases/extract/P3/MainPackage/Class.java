87159f237aa4398d3c7a3338faa9c2670fb176af
karthikabinav <karthikabinavs@gmail.com>
2012-10-18 23:29:46 +0530

    Added answers to 2 more questions in ATOC assignment 4

---
+++ compilers Lab/lab 3/tests/TA_testcases/extract/P3/MainPackage/Class.java
@@ -0,0 +1,9 @@
+package MainPackage;
+
+public class Class {
+	public String name;
+
+	public Class(String name) {
+		this.name = name;
+	}
+}
---
+++ compilers Lab/lab 3/tests/TA_testcases/extract/P3/MainPackage/Typecheck.java
@@ -0,0 +1,33 @@
+package MainPackage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import syntaxtree.*;
+import visitor.*;
+
+public class Typecheck
+{
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		try
+		{
+			/*
+			 * Node root = new MiniJavaParser(new
+			 * FileInputStream("LinearSearch-error.java")) .Goal();
+			 */
+			Node root = new MiniJavaParser(System.in).Goal();
+			SymbolTable sym = new SymbolTable();
+			root.accept(new GJNoArguDepthFirst(sym));
+			if (SymbolTable.needsTransitive)
+			{
+				new SymbolTable().findTransitiveClosure();
+			}
+			root.accept(new GJNoArguDepthFirst_Parse2(sym));
+			System.out.print("Program type checked successfully");
+		} catch (ParseException e)
+		{
+			System.out.print("Type error");
+		}
+	}
+}
