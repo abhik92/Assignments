946c23b2e5fdd7f089b4fd0d9dd3a0e2f1ea19dc
karthikabinav <karthikabinavs@gmail.com>
2012-11-11 02:53:16 +0530

    factorial works

--- compilers Lab/lab 7/Tests/output/factorial.s
+++ compilers Lab/lab 7/Tests/output/factorial.s
@@ -11,13 +11,13 @@ move $t0 $v0
 li $a0 4
 jal _halloc
 move $t1 $v0
-move $t2 Fac_ComputeFac
+la $t2 Fac_ComputeFac
 sw $t2,0($t0)
 sw $t0,0($t1)
 move $t0 $t1
 lw $t1 0( $t0)
 lw $t2 0( $t1)
-move $t1 10
+li $t1 10
 move $a0 $t0
 move $a1 $t1
 jalr $t2
@@ -39,17 +39,17 @@ sw $s1, 4($sp)
 sw $s2, 8($sp)
 move $s0 $a0
 move $s1 $a1
-move $t0 1
+li $t0 1
 slt $t1,  $s1 , $t0
 beqz $t1 L2
-move $s2 1
+li $s2 1
 b L3
 L2: 
 nop
 move $t0 $s0
 lw $t1 0( $t0)
 lw $t2 0( $t1)
-move $t1 1
+li $t1 1
 sub $t3,  $s1 , $t1
 move $a0 $t0
 move $a1 $t3
@@ -63,6 +63,10 @@ move $v0 $s2
 lw $s0, 0($sp)
 lw $s1, 4($sp)
 lw $s2, 8($sp)
+lw $ra, -4($fp)
+lw $fp, 12($sp)
+addu $sp, $sp, 20
+j $ra
 .text 
 .globl _halloc
 	_halloc:
