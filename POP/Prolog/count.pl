9317a545f69b558e077eb26580972e89e81c53a9
karthikabinav <karthikabinavs@gmail.com>
2012-11-11 15:21:28 +0530

    Added q3 and q4 in ATOC assignment 6

--- POP/Prolog/count.pl
+++ POP/Prolog/count.pl
@@ -1,5 +1,2 @@
-reverse([X|Xs],Ys):-reverse(Xs,Y1),append(Y1,[X],Ys).
-reverse([],[]).
-
-append(X,Y,Z):-Z=[H|T],append(T,Y,Z1),Z=[H|Z1].
 append([],Y,Y).
+append(X,Y,Z):- X = [H|T] , append(T,Y,Z1) , Z = [H|Z1].
